0c48f73916f78032ab58f94985b9f3f2883a080a
SoleneOclock <solene@oclock.io>
2025-02-05 18:02:15 +0100

    Deploy netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,5 +6,5 @@ export default defineConfig({
 	plugins: [react()],
 	// faut ajouter cette ligne pour que le site soit accessible sur github pages
 	// ça permet de dire à vite que le site sera accessible sur https://<username>.github.io/<repo>/ et pas sur https://<username>.github.io/
-	base: "/ci-cd/",
+	//base: "/ci-cd/",
 });
